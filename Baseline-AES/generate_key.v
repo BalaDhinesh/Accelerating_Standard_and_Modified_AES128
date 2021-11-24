289271c0c853ff3e2b2711b47a56f34f1cb590e3
rsharanesh2002 <ee19b116@smail.iitm.ac.in>
2021-11-24 10:16:48 +0530

    update

--- Baseline-AES/generate_key.v
+++ Baseline-AES/generate_key.v
@@ -1,4 +1,12 @@
-module GENERATE_KEY(rc,key,keyout);
-   input [3:0] rc;
-   input [127:0]key;
-   output [127:0] keyout;
+module GENERATE_KEY(rc,keyin,keyout);
+    input [3:0] rc; // Round-Code
+    input [127:0]keyin; // 128-bit Input Key
+    output [127:0] keyout; // 128-bit Output Key
+    wire [31:0] w0,w1,w2,w3,temp; // Wires for intermediate connections
+
+    assign w0 = keyin[127:96];
+    assign w1 = keyin[95:64];
+    assign w2 = keyin[63:32];
+    assign w3 = keyin[31:0];
+
+    
